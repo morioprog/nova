ea445e1475c990f72640ac3992088721958a8ccd
morioprog <morio.prog@gmail.com>
2024-03-19 23:43:43 +0900

    refactor: move `Color` trait

--- core/src/color.rs
+++ core/src/color.rs
@@ -1,5 +1,11 @@
-mod color;
 mod puyo_color;
 mod real_color;
 
-pub use self::{color::Color, puyo_color::PuyoColor, real_color::RealColor};
+use std::fmt::Debug;
+
+pub use self::{puyo_color::PuyoColor, real_color::RealColor};
+
+pub trait Color: Clone + Copy + PartialEq<Self> + Debug + Default {
+    fn to_char(&self) -> char;
+    fn is_normal_color(&self) -> bool;
+}
--- core/src/color/color.rs
+++
@@ -1,6 +0,0 @@
-use std::fmt::Debug;
-
-pub trait Color: Clone + Copy + PartialEq<Self> + Debug + Default {
-    fn to_char(&self) -> char;
-    fn is_normal_color(&self) -> bool;
-}
