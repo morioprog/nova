87c7a38870953170792f6fbf661727f150683443
morioprog <morio.prog@gmail.com>
2024-03-23 01:00:17 +0900

    test: add missing tests for x86_64

--- core/src/board/boardbits/x86_64.rs
+++ core/src/board/boardbits/x86_64.rs
@@ -137,9 +137,9 @@ impl BoardOps for BoardBits {
         (lh[0], lh[1])
     }
 
-    // TODO: AVX512 has _mm_srlv_epi16
     fn after_pop_mask(popped: Self) -> (u64, u64) {
         let lxhx: u64x4 = unsafe {
+            // TODO: AVX512 has _mm_srlv_epi16
             let shift = _mm256_cvtepu16_epi32(popped.popcount_u16x8().0);
             let half_ones = _mm256_cvtepu16_epi32(Self::full_mask().0);
             let shifted = _mm256_srlv_epi32(half_ones, shift);
@@ -295,6 +295,17 @@ mod tests {
     }
 
     #[test]
+    fn onebit() {
+        assert_eq!(
+            BoardBits::onebit(3, 2),
+            BoardBits::from(concat!(
+                "..1...", // 2
+                "......", // 1
+            )),
+        )
+    }
+
+    #[test]
     fn from_str() {
         let bb = BoardBits::from(concat!(
             "11..1.", // 2
@@ -316,17 +327,6 @@ mod tests {
     }
 
     #[test]
-    fn onebit() {
-        assert_eq!(
-            BoardBits::onebit(3, 2),
-            BoardBits::from(concat!(
-                "..1...", // 2
-                "......", // 1
-            )),
-        )
-    }
-
-    #[test]
     fn bit_ops() {
         let lhs = BoardBits::from("0011..");
         let rhs = BoardBits::from("0101..");
@@ -335,6 +335,24 @@ mod tests {
         assert_eq!(lhs | rhs, BoardBits::from("0111.."));
         assert_eq!(lhs ^ rhs, BoardBits::from("0110.."));
         assert_eq!(lhs.andnot(rhs), BoardBits::from("0100.."))
+    }
+
+    #[test]
+    fn shift_basic() {
+        let bb = BoardBits::onebit(2, 3);
+
+        assert_eq!(bb.shift_up(), BoardBits::onebit(2, 4));
+        assert_eq!(bb.shift_down(), BoardBits::onebit(2, 2));
+        assert_eq!(bb.shift_left(), BoardBits::onebit(1, 3));
+        assert_eq!(bb.shift_right(), BoardBits::onebit(3, 3));
+    }
+
+    #[test]
+    fn shift_corner() {
+        assert!(BoardBits::onebit(3, 15).shift_up().is_zero());
+        assert!(BoardBits::onebit(3, 0).shift_down().is_zero());
+        assert!(BoardBits::onebit(0, 3).shift_left().is_zero());
+        assert!(BoardBits::onebit(7, 3).shift_right().is_zero());
     }
 
     #[test]
@@ -381,49 +399,215 @@ mod tests {
 
     #[test]
     fn max_u16x8() {
-        let bb1: BoardBits = unsafe {
-            mem::transmute(u16x8::from_array([
-                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-            ]))
-        };
-        let bb2: BoardBits = unsafe {
-            mem::transmute(u16x8::from_array([
-                0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
-            ]))
-        };
-        let bb3: BoardBits = unsafe {
-            mem::transmute(u16x8::from_array([
-                0xABC7, 0xABC3, 0xABC4, 0xABC0, 0xABC6, 0xABC1, 0xABC2, 0xABC5,
-            ]))
-        };
-        let bb4: BoardBits = unsafe {
-            mem::transmute(u16x8::from_array([
-                0xABCD, 0x0000, 0x0001, 0xFFFF, 0xDEAD, 0xBEEF, 0x000F, 0xFFFE,
-            ]))
-        };
+        let testcases = [
+            (
+                [
+                    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                ],
+                0x0000,
+            ),
+            (
+                [
+                    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
+                ],
+                0xFFFF,
+            ),
+            (
+                [
+                    0xABC7, 0xABC3, 0xABC4, 0xABC0, 0xABC6, 0xABC1, 0xABC2, 0xABC5,
+                ],
+                0xABC7,
+            ),
+            (
+                [
+                    0xABCD, 0x0000, 0x0001, 0xFFFF, 0xDEAD, 0xBEEF, 0x000F, 0xFFFE,
+                ],
+                0xFFFF,
+            ),
+        ];
 
-        assert_eq!(bb1.max_u16x8(), 0x0000);
-        assert_eq!(bb2.max_u16x8(), 0xFFFF);
-        assert_eq!(bb3.max_u16x8(), 0xABC7);
-        assert_eq!(bb4.max_u16x8(), 0xFFFF);
+        for (bits, max) in testcases {
+            let bb: BoardBits = unsafe { mem::transmute(u16x8::from_array(bits)) };
+            assert_eq!(bb.max_u16x8(), max);
+        }
     }
 
     #[test]
-    fn shift_basic() {
-        let bb = BoardBits::onebit(2, 3);
+    fn popcount_u16x8() {
+        let testcases = [
+            (
+                [
+                    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                ],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+            ),
+            (
+                [
+                    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
+                ],
+                [16, 16, 16, 16, 16, 16, 16, 16],
+            ),
+            (
+                [
+                    0xABC7, 0xABC3, 0xABC4, 0xABC0, 0xABC6, 0xABC1, 0xABC2, 0xABC5,
+                ],
+                [10, 9, 8, 7, 9, 8, 8, 9],
+            ),
+            (
+                [
+                    0xABCD, 0x0000, 0x0001, 0xFFFF, 0xDEAD, 0xBEEF, 0x000F, 0xFFFE,
+                ],
+                [10, 0, 1, 16, 11, 13, 4, 15],
+            ),
+        ];
 
-        assert_eq!(bb.shift_up(), BoardBits::onebit(2, 4));
-        assert_eq!(bb.shift_down(), BoardBits::onebit(2, 2));
-        assert_eq!(bb.shift_left(), BoardBits::onebit(1, 3));
-        assert_eq!(bb.shift_right(), BoardBits::onebit(3, 3));
+        for (bits, max) in testcases {
+            let bb: BoardBits = unsafe { mem::transmute(u16x8::from_array(bits)) };
+            let expected: BoardBits = unsafe { mem::transmute(u16x8::from_array(max)) };
+            assert_eq!(bb.popcount_u16x8(), expected);
+        }
     }
 
     #[test]
-    fn shift_corner() {
-        assert!(BoardBits::onebit(3, 15).shift_up().is_zero());
-        assert!(BoardBits::onebit(3, 0).shift_down().is_zero());
-        assert!(BoardBits::onebit(0, 3).shift_left().is_zero());
-        assert!(BoardBits::onebit(7, 3).shift_right().is_zero());
+    fn set_below_top_one_u16x8() {
+        let testcases = [
+            (
+                [
+                    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                ],
+                [
+                    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                ],
+            ),
+            (
+                [
+                    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
+                ],
+                [
+                    0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF,
+                ],
+            ),
+            (
+                [
+                    0x00BB, 0x0000, 0x0001, 0x06F0, 0xDEAD, 0x01E2, 0x0503, 0xFFFE,
+                ],
+                [
+                    0x00FF, 0x0000, 0x0001, 0x07FF, 0xFFFF, 0x01FF, 0x07FF, 0xFFFF,
+                ],
+            ),
+        ];
+
+        for (bits, spl) in testcases {
+            let bb: BoardBits = unsafe { mem::transmute(u16x8::from_array(bits)) };
+            let expected: BoardBits = unsafe { mem::transmute(u16x8::from_array(spl)) };
+            assert_eq!(bb.set_below_top_one_u16x8(), expected);
+        }
+    }
+
+    #[test]
+    fn pext_u64() {
+        assert_eq!(
+            BoardBits::pext_u64(0b1010_1100_1010_1011, 0b0101_0110_0101_1111),
+            0b0010001011
+        );
+        assert_eq!(
+            BoardBits::pext_u64(0b0110_1001_1011_1111, 0b0111_1100_1100_0110),
+            0b110101011
+        );
+        assert_eq!(BoardBits::pext_u64(0b0110_1001_1011_1111, 0), 0);
+        assert_eq!(
+            BoardBits::pext_u64(0b0110_1001_1011_1111, u64::MAX),
+            0b0110_1001_1011_1111
+        );
+        assert_eq!(BoardBits::pext_u64(0, 0), 0);
+        assert_eq!(BoardBits::pext_u64(0, u64::MAX), 0);
+        assert_eq!(BoardBits::pext_u64(u64::MAX, 0), 0);
+        assert_eq!(BoardBits::pext_u64(u64::MAX, u64::MAX), u64::MAX);
+    }
+
+    #[test]
+    fn pdep_u64() {
+        assert_eq!(
+            BoardBits::pdep_u64(0b1010_1100_1010_1011, 0b0101_0110_0101_1111),
+            0b0000_0100_0100_1011
+        );
+        assert_eq!(
+            BoardBits::pdep_u64(0b0110_1001_1011_1111, 0b0111_1100_1100_0110),
+            0b0110_1100_1100_0110
+        );
+        assert_eq!(BoardBits::pdep_u64(0b0110_1001_1011_1111, 0), 0);
+        assert_eq!(
+            BoardBits::pdep_u64(0b0110_1001_1011_1111, u64::MAX),
+            0b0110_1001_1011_1111
+        );
+        assert_eq!(BoardBits::pdep_u64(0, 0), 0);
+        assert_eq!(BoardBits::pdep_u64(0, u64::MAX), 0);
+        assert_eq!(BoardBits::pdep_u64(u64::MAX, 0), 0);
+        assert_eq!(BoardBits::pdep_u64(u64::MAX, u64::MAX), u64::MAX);
+    }
+
+    #[test]
+    fn before_pop_mask() {
+        assert_eq!(
+            BoardBits::before_pop_mask(
+                BoardBits::from(concat!(
+                    "1..111", // 3
+                    "11.1..", // 2
+                    ".1.1..", // 1
+                )) ^ BoardBits::wall()
+            ),
+            unsafe {
+                mem::transmute(BoardBits::from(concat!(
+                    "111111", // 14
+                    "111111", // 13
+                    "111111", // 12
+                    "111111", // 11
+                    "111111", // 10
+                    "111111", // 9
+                    "111111", // 8
+                    "111111", // 7
+                    "111111", // 6
+                    "111111", // 5
+                    "111111", // 4
+                    ".11...", // 3
+                    "..1.11", // 2
+                    "1.1.11", // 1
+                )))
+            }
+        );
+    }
+
+    #[test]
+    fn after_pop_mask() {
+        assert_eq!(
+            BoardBits::after_pop_mask(BoardBits::from(concat!(
+                "1..111", // 3
+                "11.1..", // 2
+                ".1.1..", // 1
+            ))),
+            unsafe {
+                mem::transmute(
+                    (BoardBits::from(concat!(
+                        "..1...", // 14
+                        "..1.11", // 13
+                        "111.11", // 12
+                        "111111", // 11
+                        "111111", // 10
+                        "111111", // 9
+                        "111111", // 8
+                        "111111", // 7
+                        "111111", // 6
+                        "111111", // 5
+                        "111111", // 4
+                        "111111", // 3
+                        "111111", // 2
+                        "111111", // 1
+                    )) ^ BoardBits::wall())
+                    .shift_up()
+                        | BoardBits::wall().shift_up().shift_down(), // bottom-most row
+                )
+            },
+        );
     }
 
     #[test]
@@ -443,11 +627,4 @@ mod tests {
         assert!(BoardBits::zero().is_zero());
         assert!(!BoardBits::onebit(2, 3).is_zero());
     }
-
-    // TODO: add test for pext_u64
-    // TODO: add test for pdep_u64
-    // TODO: add test for before_pop_mask
-    // TODO: add test for after_pop_mask
-    // TODO: add test for popcount_u16x8
-    // TODO: add test for set_below_top_one_u16x8
 }
