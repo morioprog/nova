0f0d1e7228a5e99dfdf17b1c4f87852730f1ea3d
morioprog <morio.prog@gmail.com>
2024-07-13 22:16:30 +0900

    feat: panic if bot returned empty placements

--- bot/src/bot.rs
+++ bot/src/bot.rs
@@ -14,12 +14,20 @@ pub trait Bot {
 
     fn think_1p(&self, player_state: &PlayerState) -> Decision {
         let start = Instant::now();
-        self.think_internal_1p(player_state)
-            .with_elapsed(start.elapsed())
+        let decision = self
+            .think_internal_1p(player_state)
+            .with_elapsed(start.elapsed());
+        assert!(!decision.placements.is_empty());
+
+        decision
     }
     fn think_2p(&self, player_state_1p: &PlayerState, player_state_2p: &PlayerState) -> Decision {
         let start = Instant::now();
-        self.think_internal_2p(player_state_1p, player_state_2p)
-            .with_elapsed(start.elapsed())
+        let decision = self
+            .think_internal_2p(player_state_1p, player_state_2p)
+            .with_elapsed(start.elapsed());
+        assert!(!decision.placements.is_empty());
+
+        decision
     }
 }
